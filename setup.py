0dc2b727bac5581ca7ff619c9d58b5d92e8eb1cf
Andrea Crotti <andrea.crotti.0@gmail.com>
2013-10-27 22:07:00 +0000

    find_packages and more doc

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package = 'Smartconf'
 version = '0.1'
 
 setup(name=package,
+      author="Andrea Crotti",
+      license='BSD',
+      keywords="configuration",
+      packages=find_packages('.'),
       version=version,
-      description="Simple wrapper around ConfigParser")
+      description="Simple wrapper around ConfigParser",
+      classifiers=[
+          "Development Status :: 4 Beta",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: BSD License",
+      ]
+)
